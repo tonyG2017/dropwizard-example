4b44f9c062f623190e7cdfdbd76b6962b6244bbe
高赞 <gaozan@gz.local>
2020-08-17 13:25:01 +0800

    Update /people with optional filters.

--- src/main/java/tony/io/dropwizard/db/PeopleDAO.java
+++ src/main/java/tony/io/dropwizard/db/PeopleDAO.java
@@ -7,6 +7,8 @@ import io.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +20,26 @@ public class PeopleDAO extends AbstractDAO<Person> {
 
     public Optional<Person> findById(Long id) {
         return Optional.ofNullable(get(id));
+    }
+
+
+    //https://blog.csdn.net/qq_35224639/article/details/80611540
+    public List<Person> findWithFilters(Optional<String> fullName, Optional<String > jobTitle){
+//        Criteria c = criteria().add(Restrictions.eq("name", name));
+//        return list(c);
+//    }
+        CriteriaQuery<Person> criteriaQuery = criteriaQuery();
+
+        Root<Person> from = criteriaQuery.from(Person.class);
+        fullName.ifPresent(fullNameStr->{
+            criteriaQuery.select(from).where(from.get("fullName").in(fullNameStr));
+        });
+
+        jobTitle.ifPresent(jobTitleStr->{
+            criteriaQuery.select(from).where(from.get("jobTitle").in(jobTitleStr));
+        });
+        List<Person> personList = list(criteriaQuery);
+        return personList;
     }
 
     public Person create(Person person) {
